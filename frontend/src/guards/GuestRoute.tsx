b4ce02df4ce0a3ec267eb3a53d357367fd838267
Joshua Bolasa <bolasajoshua73@gmail.com>
2026-05-01 15:46:28 +0800

    Created: GuestRoute

---
+++ frontend/src/guards/GuestRoute.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { Navigate } from "react-router-dom";
+
+export default function GuestRoute({ children }: { children: ReactNode }) {
+    const { user, loading } = useAuth();
+
+    if (loading) return <div>Loading...</div>; // Or a spinner
+    if (user) return <Navigate to="/dashboard" replace />;  
+
+    return children;
+}
