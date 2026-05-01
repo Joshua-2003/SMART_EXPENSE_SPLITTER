ddb292135282b599e12269aa56ce88502825ca3e
Joshua Bolasa <bolasajoshua73@gmail.com>
2026-05-01 15:47:22 +0800

    defining the protected and guest route

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,13 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import LoginPage from "../pages/auth/Login";
 import ProtectedRoute from "../guards/ProtectedRoute";
+import GuestRoute from "../guards/GuestRoute";
+
+// auth
+import LoginPage from "../pages/auth/Login";
+
+// dashboard
+import DashboardPage from "../pages/dashboard/Dashboard";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                
+                <Route
+                    path="/login" 
+                    element={
+                        <GuestRoute>
+                            <LoginPage />
+                        </GuestRoute>
+                    } 
+                />
+
+                <Route 
+                    path="/dashboard"
+                    element={
+                        <ProtectedRoute>
+                            <DashboardPage />
+                        </ProtectedRoute>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     )
